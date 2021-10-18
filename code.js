329881682918104db458c9c6b57cf03a24c3f764
Pavel Yakupov <pavel.yakupov@software-kit.ru>
2021-10-18 11:52:15 +0300

    add to the cold style and add defer to index.html. Вам теперь надо доделать стили для body и поставить картинки для других температур

--- code.js
+++ code.js
@@ -1,16 +1,18 @@
-const message = prompt('Какая температура на данный момент у вас за окном?', '');
-
-
+const message = prompt(
+  "Какая температура на данный момент у вас за окном?",
+  ""
+);
 
 if (message <= 5) {
-    alert('довольно холодно');
-} else
-if (message <= 14) {
-    alert('достаточно прохладно');
+  alert("довольно холодно");
+  document.body.style.backgroundImage =
+    "url(https://cameralabs.org/media/k2/items/cache/a3614e124fb0dc2f1795684e658b9fa9_L.jpg)";
+} else if (message <= 14) {
+  alert("достаточно прохладно");
 } else if (message <= 25) {
-    alert('комфортная температура');
+  alert("комфортная температура");
 } else if (message <= 30) {
-    alert('довольно таки жарко');
+  alert("довольно таки жарко");
 } else if (message <= 40) {
-    alert('очень жарко');
-};
+  alert("очень жарко");
+}
